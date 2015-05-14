2e9233b190e4a895a7d624fbd4b6150b67f11573
teqn0x <teqn0x@gmail.com>
2015-05-14 23:22:29 +0200

    Socket messaging codes, first iteration

--- TestPythonSocket/MessageClient.py
+++ TestPythonSocket/MessageClient.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python
+
+
+import sys,os,socket
+
+def main():
+
+    #Arguments given are IP and port
+
+    argList = sys.argv
+
+    if len(argList) == 1:
+        sys.exit('Give IP and port as arguments')
+    else if len(argList) == 2:
+        tcp_ip   = argList[1].split(":")[0]
+        tcp_port = argList[1].split(":")[1]
+    else if len(argList) == 3:
+        tcp_ip   = argList[1]
+        tcp_port = argList[2]
+    else:
+        sys.exit('Too many arguments given')
+    
+
+    buffersize = 1024
+    message = input('Type a message to send')
+
+
+    sock = socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock.connect((tcp_ip,tpc_port))
+    sock.send(message)
+    data = sock.recv(buffersize)
+    sock.close()
+
+    print 'Received data:', data
+
+if __name__ == "__main__":
+    main()
+
+
+    
+    
+    
--- TestPythonSocket/MessageServer.py
+++ TestPythonSocket/MessageServer.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python
+
+import sys,os,socket
+
+def main():
+
+    argList = sys.argv
+
+    if argList == 1:
+        sys.exit('Give IP and port to listen to')
+    else if argList == 2:
+        tcp_ip   = argList[1].split(":")[0]
+        tcp_port = argList[1].split(":")[1]
+    else if argList == 3:
+        tcp_ip   = argList[1]
+        tcp_port = argList[2]
+    else:
+        sys.exit('Too many arguments are given')
+
+    buffersize = 20
+    sock = socket.socket(socket.AF_NET,socket.SOCK_STREAM)
+    sock.bind((tcp_ip,tcp_port))
+    sock.listen(1)
+
+    conn, addr = sock.accept()
+
+    print 'Connection address:', addr
+    while True:
+        data = conn.recv(BUFFER_SIZE)
+
+        if not data: break
+        print "Received data:", data
+        conn.send(data)
+    conn.close()
+
+    if __name__ == "__main__":
+        main()
+
